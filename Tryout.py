1302153ae8f2c947d87988d8b850e3b97195501d
MJEHiemstra <95851358+MJEHiemstra@users.noreply.github.com>
2021-12-09 10:41:21 +0100

    Change made on the remote server

--- Tryout.py
+++ Tryout.py
@@ -1,3 +1,4 @@
 print("My own project from github")
 print("If I commit, not yet shown in github")
 print("If I commit & push, it is visible in github")
+print("This is written in github as if it is written by a teammate")
